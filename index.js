647163b37c1799d119c59541ae90b6e6be12e843
Korakrit <korakrit.char@outlook.com>
2024-05-28 20:41:49 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const todoListRoutes = require("./routes/todoList");
+const userRoutes = require("./routes/user");
+const db = require("./models");
+require("./config/passport/passport");
+
+app.use(cors());
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use("/todo-list", todoListRoutes);
+app.use("/users", userRoutes);
+
+db.sequelize.sync({ force: false }).then(() => {
+  app.listen(process.env.PORT, () => {
+    console.log(`Server is running at port ${process.env.PORT}`);
+  });
+});
